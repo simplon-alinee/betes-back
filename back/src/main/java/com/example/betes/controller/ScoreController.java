329a4787f2aaed1f7177427bec5dbaa0f0ab059a
Thibault DUMAS <32924124+Thibalti@users.noreply.github.com>
2020-08-25 12:46:27 +0200

    add methode get allscore by score desc

--- back/src/main/java/com/example/betes/controller/ScoreController.java
+++ back/src/main/java/com/example/betes/controller/ScoreController.java
@@ -3,10 +3,9 @@ package com.example.betes.controller;
 import com.example.betes.model.Score;
 import com.example.betes.service.ScoreService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/score")
@@ -19,5 +18,14 @@ public class ScoreController {
     @GetMapping("/{id}")
     public Score getById(@PathVariable("id") Long id) {
         return scoreService.getById(id);
+    }
+
+    @GetMapping("/all")
+    public Page<Score> findAllScores(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "size", defaultValue = "10") Integer size,
+            @RequestParam(value = "sortProperty", defaultValue = "score") String sortProperty,
+            @RequestParam(value = "sortDirection", defaultValue = "DESC") Sort.Direction sortDirection) {
+        return scoreService.findAllScores(page, size, sortProperty, sortDirection);
     }
 }
